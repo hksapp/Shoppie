509176574a6145808dba279b89c0ffa44786d883
Kamal <kamalakar.sambaraj@gmai.com>
2017-03-08 09:30:54 +0530

    Uploading username , email , userid

--- app/src/main/java/com/hkapps/shoppie/MainActivity.java
+++ app/src/main/java/com/hkapps/shoppie/MainActivity.java
@@ -3,14 +3,10 @@ package com.hkapps.shoppie;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -18,13 +14,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 
@@ -143,6 +140,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show();
 
+
+
             }else if(resultCode == RESULT_CANCELED) {
 
                 Toast.makeText(this, "Sign in Cancelled", Toast.LENGTH_SHORT).show();
@@ -194,6 +193,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (user != null) {
                     // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+
+                    DatabaseReference uploadUserData = FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
+                    uploadUserData.child("email").setValue(user.getEmail().toString());
+                    uploadUserData.child("username").setValue(user.getDisplayName().toString());
+                    uploadUserData.child("userid").setValue(user.getUid().toString());
+
+
                 } else {
 
                     // User is signed out
