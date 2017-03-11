329a934f26b623cc2db448527a6e554975417527
vamshi <vamshi.guntupalli@gmail.com>
2017-03-11 22:49:43 +0530

    got username and emailid

--- app/src/main/java/com/hkapps/shoppie/profile.java
+++ app/src/main/java/com/hkapps/shoppie/profile.java
@@ -2,12 +2,38 @@ package com.hkapps.shoppie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class profile extends AppCompatActivity {
-
+    private TextView mailid,username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+         mailid =(TextView)findViewById(R.id.mailid);
+        username=(TextView)findViewById(R.id.username);
+        final FirebaseDatabase database= FirebaseDatabase.getInstance();
+        DatabaseReference ref=database.getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid().toString());
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                mailid.setText(dataSnapshot.child("email").getValue().toString());
+                username.setText(dataSnapshot.child("username").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
