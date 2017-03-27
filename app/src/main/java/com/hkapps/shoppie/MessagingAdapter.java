02e6840a5a2b6975b2e338d3639a470d8a4e9b0e
vamshi <vamshi.guntupalli@gmail.com>
2017-03-27 15:33:49 +0530

    ui code some changes

--- app/src/main/java/com/hkapps/shoppie/MessagingAdapter.java
+++ app/src/main/java/com/hkapps/shoppie/MessagingAdapter.java
@@ -20,8 +20,8 @@ import com.squareup.picasso.Picasso;
  * Created by vamshi on 17-03-2017.
  */
 
-public class MessagingAdapter extends FirebaseRecyclerAdapter<MessagingObject,MessagingHolder> {
-    public static String TAG=MessagingAdapter.class.getSimpleName();
+public class MessagingAdapter extends FirebaseRecyclerAdapter<MessagingObject, MessagingHolder> {
+    public static String TAG = MessagingAdapter.class.getSimpleName();
     private Context context;
     private DatabaseReference ref;
     private static String username;
@@ -34,22 +34,22 @@ public class MessagingAdapter extends FirebaseRecyclerAdapter<MessagingObject,Me
      * @param ref             The Firebase location to watch for data changes. Can also be a slice of a location, using some
      *                        combination of {@code limit()}, {@code startAt()}, and {@code endAt()}.
      */
-    public MessagingAdapter(Class<MessagingObject> modelClass, int modelLayout, Class<MessagingHolder> viewHolderClass, Query ref,Context context) {
+    public MessagingAdapter(Class<MessagingObject> modelClass, int modelLayout, Class<MessagingHolder> viewHolderClass, Query ref, Context context) {
         super(modelClass, modelLayout, viewHolderClass, ref);
-        this.context=context;
+        this.context = context;
     }
 
     @Override
     protected void populateViewHolder(MessagingHolder viewHolder, MessagingObject model, int position) {
-        String user=model.getSentBy();
-        String mainuser= FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
-        String mtype=model.getMessageType().toString();
+        String user = model.getSentBy();
+        String mainuser = FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
+        String mtype = model.getMessageType().toString();
 
-        ref= FirebaseDatabase.getInstance().getReference().child("Users").child(model.getSentBy().toString());
+        ref = FirebaseDatabase.getInstance().getReference().child("Users").child(model.getSentBy().toString());
         ref.child("username").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-               username=dataSnapshot.getValue().toString();
+                username = dataSnapshot.getValue().toString();
             }
 
             @Override
@@ -58,29 +58,29 @@ public class MessagingAdapter extends FirebaseRecyclerAdapter<MessagingObject,Me
             }
         });
         viewHolder.sentby_username.setText(username);
-        if(mtype.equals("text")){
-        viewHolder.message.setText(model.getMessage());
-        viewHolder.image.setVisibility(View.GONE);
+        if (mtype.equals("text")) {
+            viewHolder.message.setText(model.getMessage());
+            viewHolder.image.setVisibility(View.GONE);
             viewHolder.message.setVisibility(View.VISIBLE);
-            if(user.equals(mainuser)){
+            if (user.equals(mainuser)) {
                 viewHolder.message_layout.setGravity(Gravity.END);
-                viewHolder.message_layout.setPadding(0,0,25,0);
+                viewHolder.message_layout.setPadding(0, 0, 25, 0);
+            } else {
+                viewHolder.message_layout.setPadding(25, 0, 0, 0);
             }
-            else{
-                viewHolder.message_layout.setPadding(25,0,0,0);
-            }}
-        if(mtype.equals("image")){
+        }
+        if (mtype.equals("image")) {
             viewHolder.message.setVisibility(View.GONE);
             viewHolder.image.setVisibility(View.VISIBLE);
             Picasso.with(context).load(model.getMessage()).into(viewHolder.image);
-            if(user.equals(mainuser)){
-                /*LinearLayout.LayoutParams params ;
-                params=(LinearLayout.LayoutParams)viewHolder.message_layout.getLayoutParams();
-                params.gravity=Gravity.RIGHT;*/
-                viewHolder.message_layout.setPadding(0,0,25,0);
-            }
-            else{
-                viewHolder.message_layout.setPadding(25,0,0,0);
+            if (user.equals(mainuser)) {
+               /* LinearLayout.LayoutParams params;
+                params = (LinearLayout.LayoutParams) viewHolder.message_layout.getLayoutParams();
+                params.gravity = Gravity.RIGHT;*/
+                viewHolder.message_layout.setPadding(0, 0, 25, 0);
+            } else
+            {
+                viewHolder.message_layout.setPadding(25, 0, 0, 0);
             }
         }
 
